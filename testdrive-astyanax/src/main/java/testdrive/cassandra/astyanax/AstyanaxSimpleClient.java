35682693b94e24ae0e964ab2c08d10b993435503
halida9cxm <halida9cxm@gmail.com>
2014-06-24 17:43:29 +0800

    testdrive astyanax

--- testdrive-astyanax/src/main/java/testdrive/cassandra/astyanax/AstyanaxSimpleClient.java
+++ testdrive-astyanax/src/main/java/testdrive/cassandra/astyanax/AstyanaxSimpleClient.java
@@ -6,20 +6,24 @@ import java.util.List;
 import com.google.common.base.Supplier;
 import com.netflix.astyanax.AstyanaxContext;
 import com.netflix.astyanax.Keyspace;
+import com.netflix.astyanax.MutationBatch;
 import com.netflix.astyanax.connectionpool.Host;
 import com.netflix.astyanax.connectionpool.NodeDiscoveryType;
 import com.netflix.astyanax.cql.CqlFamilyFactory;
 import com.netflix.astyanax.cql.JavaDriverConfigBuilder;
 import com.netflix.astyanax.impl.AstyanaxConfigurationImpl;
+import com.netflix.astyanax.model.Column;
 import com.netflix.astyanax.model.ColumnFamily;
+import com.netflix.astyanax.query.ColumnFamilyQuery;
 import com.netflix.astyanax.serializers.IntegerSerializer;
 import com.netflix.astyanax.serializers.StringSerializer;
 
 public class AstyanaxSimpleClient {
-
+	AstyanaxContext<Keyspace> context;
 	private Keyspace keyspace;
+	private ColumnFamily<Integer, String> employees;
 
-	public void initialize() {
+	public void initialize() throws Exception {
 		final String clusterName = "Test Cluster";
 		final String keyspaceName = "simplex";
 
@@ -32,7 +36,7 @@ public class AstyanaxSimpleClient {
 			}
 		};
 
-		AstyanaxContext<Keyspace> context = new AstyanaxContext.Builder()
+		context = new AstyanaxContext.Builder()
 				.forCluster(clusterName)
 				.forKeyspace(keyspaceName)
 				.withHostSupplier(HostSupplier)
@@ -44,21 +48,49 @@ public class AstyanaxSimpleClient {
 						new JavaDriverConfigBuilder().build())
 				.buildKeyspace(CqlFamilyFactory.getInstance());
 
+		context.start();
 		keyspace = context.getClient();
+
+		employees = ColumnFamily.newColumnFamily("employeeinfo",
+				IntegerSerializer.get(), StringSerializer.get());
+		employees.describe(keyspace);
+	}
+
+	public void close() {
+		context.shutdown();
 	}
 
 	public void query() throws Exception {
-		ColumnFamily<Integer, String> CF_EMPLOYEES = new ColumnFamily<Integer, String>(
-				"EmployeeInfo", IntegerSerializer.get(), StringSerializer.get());
-		keyspace.prepareQuery(CF_EMPLOYEES)
-				.withCql(
-						"CREATE TABLE EmployeeInfo (id int PRIMARY KEY, fname text, lname text, age int, salary bigint)")
-				.execute();
+		ColumnFamilyQuery<Integer, String> query = keyspace
+				.prepareQuery(employees);
+		Column<String> column = query.getKey(1).getColumn("lname").execute()
+				.getResult();
+		System.out.println("last name = " + column.getStringValue());
+	}
+
+	public void update() {
+		MutationBatch m = keyspace.prepareMutationBatch();
+		m.withRow(employees, 1).putColumn("lname", "Bob");
+		try {
+			m.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) throws Exception {
 		AstyanaxSimpleClient client = new AstyanaxSimpleClient();
 		client.initialize();
-		client.query();
+		try {
+			System.out.println("# before update");
+			client.query();
+			client.update();
+			System.out.println("# after update");
+			client.query();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			client.close();
+		}
 	}
 }
