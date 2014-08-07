016b300ce93aec98b66843ae13ebb32805d38681
halida9cxm <halida9cxm@gmail.com>
2014-08-07 13:54:14 +0800

    fix leader election

--- testdrive-bigdatautils/src/main/java/testdrive/bigdatautils/curator/CuratorTest.java
+++ testdrive-bigdatautils/src/main/java/testdrive/bigdatautils/curator/CuratorTest.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.leader.LeaderSelector;
 import org.apache.curator.framework.recipes.leader.LeaderSelectorListener;
 import org.apache.curator.framework.recipes.leader.LeaderSelectorListenerAdapter;
 import org.apache.curator.framework.recipes.locks.InterProcessMutex;
@@ -58,6 +59,12 @@ public class CuratorTest {
 				// this method when you want to relinquish leadership
 			}
 		};
+		String path = "/path/to/election";
+		LeaderSelector selector = new LeaderSelector(client, path, listener);
+		selector.autoRequeue(); // not required, but this is behavior that you
+								// will probably expect
+		selector.start();
+		selector.close();
 	}
 
 	public static void main(String[] args) throws Exception {
